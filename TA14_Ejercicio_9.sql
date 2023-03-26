867ab1918c0a9f4a5abb18fb9cacd62de34ca926
Francisco Parra <franparpe@disroot.org>
2023-03-26 18:34:52 +0200

    Modificaciones menores

--- TA14_Ejercicio_9.sql
+++ TA14_Ejercicio_9.sql
@@ -4,25 +4,28 @@ USE cientificos;
 
 create table cientificos(
 DNI varchar (8),
-NomApels nvarchar(255)
+NomApels nvarchar(255),
+primary key (DNI)
 );
 
 create table proyecto(
 ID char(4),
 Nombre nvarchar (255),
-Horas int
+Horas int,
+primary key (ID)
 );
 
 create table Asignado_a(
 cientifico varchar(8),
 proyecto char(4),
-primary key (cientifico),
 key (cientifico),
 foreign key (cientifico)
-references cientifico,
-primary key (proyecto),
+references cientificos (DNI)
+on delete no action on update cascade,
 key (proyecto),
 foreign key(proyecto)
-references proyecto
+references proyecto (ID)
+on delete no action
+on update cascade,
+primary key (cientifico, proyecto)
 );
-
