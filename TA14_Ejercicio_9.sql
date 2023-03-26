60602c50f43e5b028e4779ccfceb9bf9e3632c69
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-03-26 18:11:11 +0200

    Actualizar ej9

--- TA14_Ejercicio_9.sql
+++ TA14_Ejercicio_9.sql
@@ -4,13 +4,15 @@ USE cientificos;
 
 create table cientificos(
 DNI varchar (8),
-NomApels nvarchar(255)
+NomApels nvarchar(255),
+primary key (DNI)
 );
 
 create table proyecto(
 ID char(4),
 Nombre nvarchar (255),
-Horas int
+Horas int,
+primary key (ID)
 );
 
 create table Asignado_a(
@@ -24,5 +26,10 @@ primary key (proyecto),
 key (proyecto),
 foreign key(proyecto)
 references proyecto
+on delete set null
+on update cascade
 );
 
+SELECT * from cientificos;
+SELECT * from proyecto;
+select * from Asignado_a;
